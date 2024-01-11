23f0dd3ca8ddca65d0e2cd1eef176ea60166ffaf
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-01-11 16:41:03 -0700

    fix csmtp to smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  csmtp_credentials = Rails.application.credentials.smtp
+  smtp_credentials = Rails.application.credentials.smtp
 
   config.action_mailer.smtp_settings = {
     address: smtp_credentials[:address],
